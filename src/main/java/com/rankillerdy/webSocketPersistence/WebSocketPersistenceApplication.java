915d8f0f683a17d43418207c7ba0669c13f37f7a
RankillerDY <rankillerdy@gmail.com>
2024-04-12 13:25:08 +0700

    first commit

---
+++ src/main/java/com/rankillerdy/webSocketPersistence/WebSocketPersistenceApplication.java
@@ -0,0 +1,13 @@
+package com.rankillerdy.webSocketPersistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketPersistenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rankillerdy/webSocketPersistence/WebSocketPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rankillerdy.webSocketPersistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketPersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
