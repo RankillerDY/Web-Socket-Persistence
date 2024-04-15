b46e274556507ffbe0cbf960667a300f5be1adf1
Đinh Gia Bảo <107354883+RankillerDY@users.noreply.github.com>
2024-04-15 11:39:03 +0700

    chat message service implemented

--- src/main/java/com/rankillerdy/webSocketPersistence/chatroom/ChatRoomRepository.java
+++ src/main/java/com/rankillerdy/webSocketPersistence/chatroom/ChatRoomRepository.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 
 public interface ChatRoomRepository extends MongoRepository<Chatroom, String> {
     Optional<Chatroom> findBySenderIdAndRecipientId(String senderId, String recipientId);
+
 }
