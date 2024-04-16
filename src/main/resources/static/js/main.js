e31523a886b7bc5b12c12aa59392049725bae75b
Đinh Gia Bảo <107354883+RankillerDY@users.noreply.github.com>
2024-04-16 11:48:40 +0700

    connect the user and subscribe to notification

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const usernamePage = document.querySelector('#username-page');
+const chatPage = document.querySelector('#chat-page');
+const usernameForm = document.querySelector('#usernameForm');
+const messageForm = document.querySelector('#messageForm');
+const messageInput = document.querySelector('#message');
+const connectingElement = document.querySelector('.connecting');
+const chatArea = document.querySelector('#chat-messages');
+const logout = document.querySelector('#logout');
+
+let stompClient = null;
+let nickname = null;
+let fullname = null;
+let selectedUserId = null;
+
+function connect(event) {
+    nickname = document.querySelector('#nickname').value.trim();
+    fullname = document.querySelector('#fullname').value.trim();
+    if(nickname && fullname) {
+        usernamePage.classList.add('hidden');
+        chatPage.classList.remove('hidden');
+
+        const socket = new SockJS('/ws');
+        stompClient = Stomp.over(socket);
+
+        stompClient.connect({}, onConnected, onError)
+    }
+    event.preventDefault();
+}
+
+function onConnected() {
+    stompClient.subscribe(`/user/${nickname}/queue/messages`, onMessageReceived);
+    stompClient.subscribe(`/user/public`, onMessageReceived);
+
+    //register the connected user
+    stompClient.send('app/user.addUser',
+        {},
+        JSON.stringify({
+            nickname: nickname,
+            fullname: fullname,
+            status: 'ONLINE'
+        }))
+    //find and display the connected user
+
+}
+
+function onError() {
+
+}
+
+function onMessageReceived() {
+
+}
+usernameForm.addEventListener('submit', connect, true);
